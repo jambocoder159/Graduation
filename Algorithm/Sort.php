ef8c8c1ca76361d30afd848fb062694a759d3eac
swift811111 <swift811111@gmail.com>
2016-09-18 19:52:18 +0800

    add insertion sort and chart

--- Algorithm/Sort.php
+++ Algorithm/Sort.php
@@ -12,29 +12,52 @@
 		<div class="center_container">
 			<div class="input_container">
 				<div>
-					<span>範圍 : </span><input type="text" id="range" >
-				</div>
-				<div>
-					<span>數量 : </span><input type="text" id="num" >
-				</div>
-				<div>
-					<input type="button" name="" value="開始" onclick="Sort()">
+					<div class="text_container">
+						<div>
+							<span>範圍 : </span><input type="text" id="range" >
+						</div>
+						<div>
+							<span>數量 : </span><input type="text" id="num" >
+						</div>
+						<div>
+							<input type="button" name="" value="開始" onclick="Sort()">
+						</div>
+					</div>
+					<div>
+						<input type="checkbox" id="Selection"><label>Selection sort</label>
+						<input type="checkbox" id="Insertion"><label>Insertion sort</label>
+						<input type="checkbox" id="Bubble"><label>Bubble sort</label>
+						<input type="checkbox" id="Radix"><label>Radix sort</label>
+						<input type="checkbox" id="Heap"><label>Heap sort</label>
+						<input type="checkbox" id="Merge"><label>Merge sort</label><br>
+						<input type="checkbox" id="Quick"><label>Quick sort(recursive version)</label>
+						<input type="checkbox" id="non_Quick"><label>Quick sort(non-recursive version)</label>
+					</div>
 				</div>
 			</div>
 			<div class="display">
-				<div class="selection frame" >
-					<div id="name" style="color:red;">Selection Sort </div>
-					<div id="name">Sort Times : </div>
+				<div id="Selection_frame" class="frame" >
+					<div id="Selection_name" style="color:red;">Selection Sort </div>
+					<div id="Selection_name">Sort Times : </div>
 						<div id="selection_sort_times"></div>
-					<div id="name">Random Numbers : </div>
+					<div id="Selection_name">Random Numbers : </div>
 						<div id="selection_print"></div>
 				</div>
-				<div class="selection frame" >
-					<div id="name" style="color:red;">Bubble Sort </div>
-					<div id="name">Sort Times : </div>
+
+				<div id="Bubble_frame" class="frame" >
+					<div id="Bubble_name" style="color:red;">Bubble Sort </div>
+					<div id="Bubble_name">Sort Times : </div>
 						<div id="bubble_sort_times"></div>
-					<div id="name">Random Numbers : </div>
+					<div id="Bubble_name">Random Numbers : </div>
 						<div id="bubble_print"></div>
+				</div>
+
+				<div id="Insertion_frame" class="frame" >
+					<div id="Insertion_name" style="color:red;">Insertion Sort </div>
+					<div id="Insertion_name">Sort Times : </div>
+						<div id="insertion_sort_times"></div>
+					<div id="Insertion_name">Random Numbers : </div>
+						<div id="insertion_print"></div>
 				</div>
 			</div>
 		</div>
@@ -44,7 +67,5 @@
 	</div>
 
 </body>
-<script type="text/javascript">
-	
-</script>
+
 </html>
--- Algorithm/Sort_Js.js
+++ Algorithm/Sort_Js.js
@@ -1,24 +1,26 @@
-
+var cur_selection = new Array(5) ;
+var cur_bubble = new Array(5) ;
+var cur_insertion = new Array(5) ;
+var once_plus = 3000 ;
 //google chart--------------------------------------------------------------------
  google.charts.load('current', {'packages':['line']});
       google.charts.setOnLoadCallback(drawChart);
-      var x = new Array() ;
     function drawChart() {
 
       var data = new google.visualization.DataTable();
         data.addColumn('number', 'Date Size');
         data.addColumn('number', 'Selection Sort');
         data.addColumn('number', 'Bubble Sort');
-        data.addColumn('number', '按讚次數');
+        data.addColumn('number', 'Insertion Sort');
         data.addRows(5);
 
-        var size = 2000 ;
+        var size = 1000 ;
         for(var i=0; i<5; i++){
           data.setValue(i,0,size);
-          data.setValue(i,1,Math.floor(Math.random()*(299+1)+1));
-          data.setValue(i,2,Math.floor(Math.random()*(299+1)+1));
-          data.setValue(i,3,Math.floor(Math.random()*(299+1)+1));
-          size+=2000 ;
+          data.setValue(i,1,cur_selection[i]);
+          data.setValue(i,2,cur_bubble[i]);
+          data.setValue(i,3,cur_insertion[i]);
+          size += parseInt(once_plus) ;
         }
        
 
@@ -37,10 +39,37 @@
     }
 //--------------------------------------------------------------------------------    
 
+//css add and remove================================================================================================
+function add_css(frame,name) {
+    var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = "#"+frame+" { border:1px dotted red; margin-top:1%;display:block; overflow: auto;  } ";
+        document.body.appendChild(css);        
+}
+function remove_css(frame1,frame2,frame3) {
+    var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = "#"+frame1+","+"#"+frame2+","+"#"+frame3+" { display:none; }";
+        document.body.appendChild(css);        
+}
+//===================================================================================================================
 
+//creat array contain random values-------------------------------------------
+function ranges_nums(once){
 
+  var ranges = document.getElementById("range").value ; //範圍
+  var nums = document.getElementById("num").value ; //數量
+  nums = parseInt(nums) + parseInt(once) ;
 
+  var items = new Array();
+    //把random出來的數字塞進陣列
+    for(var i =0;i < nums ; i ++){
+        items.push(random_num(1,ranges)) ;    
+    }
+  return items ;  
 
+}
+//-----------------------------------------------------------------------------
 
 //random=============================================
 function random_num(min,max) {
@@ -59,97 +88,136 @@ function swap(items, firstIndex, secondIndex){
 var css_num = 0 ;// 讓網頁只執行一次匯入CSS
 
 function Sort(){
-    var ranges = document.getElementById("range").value ; //範圍
-    var nums = document.getElementById("num").value ; //數量
-
-    var items = new Array();
+  for (var i = 0; i <5; i++) {
+    cur_selection[i] = null ;
+    cur_bubble[i] = null ;
+    cur_insertion[i] = null ;
+  }
     
-    //把random出來的數字塞進陣列
-    for(var i =0;i < nums ; i ++){
-        items.push(random_num(1,ranges)) ;    
-    }
-
-    var names = document.getElementById("name") ;
-    var i ;
-    for(i=0; i<names.length ; i++){
-        names[i].style.display = 'block' ;
-    }
-
-    if(css_num == 0){
-        var css = document.createElement("style");
-        css.type = "text/css";
-        css.innerHTML = ".frame { border:1px dotted red; } #name { display:block ;}";
-        document.body.appendChild(css);
-        
-    }    
-    css_num++ ;
-
-    selectionSort(items);
-    bubbleSort(items);
+  remove_css("Selection_frame","Bubble_frame","Insertion_frame");
+  if(document.getElementById("Selection").checked == true)selectionSort();
+  if(document.getElementById("Bubble").checked == true)bubbleSort();
+  if(document.getElementById("Insertion").checked == true)insertionSort();
+  drawChart();
 }
 
 //selectionSort===================================================
-function selectionSort(items){
-    var start = 0; 
-    var end = 0;
+function selectionSort(){
 
-    start = new Date().getTime(); //測試程式開始時間
+    var once = 0 ;
 
-    var len = items.length,
-        min;
+    for(var u =0; u<5; u++){
 
-    for (i=0; i < len; i++){
+      items = ranges_nums(once);
+      add_css("Selection_frame","Selection_name");
+      var start = 0; 
+      var end = 0;
 
-        //set minimum to this position
-        min = i;
+      start = new Date().getTime(); //測試程式開始時間
 
-        //check the rest of the array to see if anything is smaller
-        for (j=i+1; j < len; j++){
-            if (items[j] < items[min]){
-                min = j;
-            }
-        }
+      var len = items.length,
+          min;
 
-        //if the minimum isn't in the position, swap it
-        if (i != min){
-            swap(items, i, min);
-        }
+      for (i=0; i < len; i++){
+
+          //set minimum to this position
+          min = i;
+
+          //check the rest of the array to see if anything is smaller
+          for (j=i+1; j < len; j++){
+              if (items[j] < items[min]){
+                  min = j;
+              }
+          }
+
+          //if the minimum isn't in the position, swap it
+          if (i != min){
+              swap(items, i, min);
+          }
+      }
+      
+      end = new Date().getTime();//測試程式結束時間
+      once += parseInt(once_plus) ;
+
+      cur_selection[u] = ((end - start) / 1000);
     }
-    
-    end = new Date().getTime();//測試程式結束時間
+
     document.getElementById("selection_print").innerHTML = items;
-    document.getElementById("selection_sort_times").innerHTML = ((end - start) / 1000) + "sec" ; //總測試時間
+    document.getElementById("selection_sort_times").innerHTML = cur_selection[0] +","+ cur_selection[1] +","+ cur_selection[2] +","+ cur_selection[3] +","+ cur_selection[4] + "sec" ; //總測試時間
 
 
 }
 //==================================================================
 
 //bubbleSort---------------------------------------------------------
-function bubbleSort(items) {
+function bubbleSort() {
+    var once = 0 ;
 
+    for(var u=0; u<5; u++){
+
+      items = ranges_nums(once);
+      add_css("Bubble_frame","Bubble_name");
+      var start = 0; 
+      var end = 0;
+
+      start = new Date().getTime(); //測試程式開始時間
+
+        var length = items.length;
+        for (var i = 0; i < length; i++) { 
+          for (var j = 0; j < (length - i - 1); j++) { 
+           
+            if(items[j] > items[j+1]) {
+              //Swap the numbers
+              swap(items,j,j+1);
+            }
+          }        
+        }
+
+       end = new Date().getTime();//測試程式結束時間
+
+      once += parseInt(once_plus) ;
+
+      cur_bubble[u] = ((end - start) / 1000);
+     }
+    document.getElementById("bubble_print").innerHTML = items;
+    document.getElementById("bubble_sort_times").innerHTML = cur_bubble[0]+","+cur_bubble[1]+","+cur_bubble[2]+","+cur_bubble[3]+","+cur_bubble[4]+","+ "sec" ; //總測試時間 
+
+}
+//-------------------------------------------------------------------
+
+//Insertion sort==================================================================
+function insertionSort(){
+  var once = 0 ;
+
+  for(var u=0; u<5; u++){
+
+    items = ranges_nums(once);
+    add_css("Insertion_frame","Insertion_name");
     var start = 0; 
     var end = 0;
 
-
     start = new Date().getTime(); //測試程式開始時間
 
-      var length = items.length;
-      for (var i = 0; i < length; i++) { //Number of passes
-        for (var j = 0; j < (length - i - 1); j++) { //Notice that j < (length - i)
-          //Compare the adjacent positions
-          if(items[j] > items[j+1]) {
-            //Swap the numbers
-            var tmp = items[j];  //Temporary variable to hold the current number
-            items[j] = items[j+1]; //Replace current number with adjacent number
-            items[j+1] = tmp; //Replace adjacent number with current number
-          }
-        }        
-      }
+    var len = items.length;
+    for (var i = 0; i < len; i++) {
+        var tmp = items[i]; 
+       
+        for (var j = i - 1; j >= 0 && (items[j] > tmp); j--) {
+            
+            items[j + 1] = items[j];
+        }
+       
+        items[j + 1] = tmp;
+    }
 
-     end = new Date().getTime();//測試程式結束時間
-    document.getElementById("bubble_print").innerHTML = items;
-    document.getElementById("bubble_sort_times").innerHTML = ((end - start) / 1000) + "sec" ; //總測試時間 
+    end = new Date().getTime();//測試程式結束時間
+    once += parseInt(once_plus) ;
 
+    cur_insertion[u] = ((end - start) / 1000);
+
+  }
+
+  document.getElementById("insertion_print").innerHTML = items;
+  document.getElementById("insertion_sort_times").innerHTML = cur_insertion[0]+","+cur_insertion[1]+","+cur_insertion[2]+","+cur_insertion[3]+","+cur_insertion[4]+","+ "sec" ; //總測試時間 
 }
-
-//-------------------------------------------------------------------
+//================================================================================
